5b7f940bac7098390d7e2b19d2f4866283ba47ee
TingHsi <rnb520@vip.qq.com>
2016-07-22 13:08:47 +0800

    解决加载完成后页面抖动问题
    参考了SUI官方示例

--- src/views/list.vue
+++ src/views/list.vue
@@ -3,11 +3,11 @@
     <nav class="bar bar-nav">
       <h1 class="title">滚动加载更多</h1>
     </nav>
-    <div class="content list"
+    <div class="content infinite-scroll infinite-scroll-bottom"
     v-infinite-scroll="loadMore">
-      <div class="list-block infinite-list">
-        <ul>
-          <li class="item-content" v-for="item in items" track-by="$index">
+      <div class="list-block">
+        <ul class="list-container">
+          <li class="item-content" v-for="item in items">
             <div class="item-media"><i class="icon icon-dianji"></i></div>
             <div class="item-inner">
               <div class="item-title">商品名称</div>
@@ -16,13 +16,17 @@
           </li>
         </ul>
       </div>
+      <!-- 加载提示符 -->
+      <div class="infinite-scroll-preloader" v-show="loading">
+          <div class="preloader"></div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import {loader} from '../util/util'
-import $ from 'zepto'
+// import {loader} from '../util/util'
+// import $ from 'zepto'
 
 export default {
   route: {
@@ -31,43 +35,57 @@ export default {
     }
   },
   ready () {
-    for (let i = 0 ; i < 15; i++) {
-      this.items.push({
-        id: i,
-        name: `demo${i + 1}`
-      })
-    }
+    // 预先加载20条
+    this.addItems(20, 0)
   },
   data () {
     return {
       items: [],
-      loading: false
+      loading: false, // 加载flag
+      maxItems: 100, // 最多可加载的条目
+      itemsPerLoad: 5, // 每次加载添加多少条目
+      lastIndex: 20 // 上次加载的序号
     }
   },
   computed: {
-    length () {
-      return this.items.length
-    }
   },
   methods: {
     loadMore () {
-      if (this.loading) {
-        return
-      }
+
+      // 如果正在加载，则退出
+      if (this.loading) return
+
+      // 设置flag
       this.loading = true
-      let scroller = $('.list')
-      loader.show()
+
+      // 模拟1.5s的加载过程
       setTimeout(() => {
-        let i = this.length
+        // 重置加载flag
+        this.loading = false
+
+        if (this.lastIndex >= this.maxItems) {
+          // TODO! 加载完毕，则注销无限加载事件，以防不必要的加载
+          // $.detachInfiniteScroll($('.infinite-scroll'))
+          // 删除加载提示符
+          // $('.infinite-scroll-preloader').remove()
+          return
+        }
+
+        // 添加新条目
+        this.addItems(this.itemsPerLoad, this.lastIndex)
+        // 更新最后加载的序号
+        this.lastIndex = this.items.length
+
+      }, 1500)
+    },
+    addItems (number, lastIndex) {
+      // 生成新条目的数据
+      for (let i = lastIndex; i < lastIndex + number; i++) {
         this.items.push({
           id: i,
           name: `demo${i + 1}`
         })
-        let scrollTop = scroller[0].scrollHeight - scroller.height() - 20
-        scroller.scrollTop(scrollTop)
-        this.loading = false
-        loader.hide()
-      }, 1500)
+      }
     }
   }
 }
@@ -77,8 +95,7 @@ export default {
 .list-block {
   margin: .5rem 0;
 }
-.list {
-  bottom: 2.5rem;
-  padding-bottom: 1rem;
+.infinite-scroll-bottom {
+  bottom: 2.2rem;
 }
 </style>
